37e4d3727f45ece8c00c0df63348d9a6c6885aea
Oliver Levine <oliversfphillips@icloud.com>
2025-09-04 08:06:25 -0700

    Complete exercises 1, 2, and 3

--- exercises.py
+++ exercises.py
@@ -23,29 +23,38 @@ example_list_function()
 # Assign the last student’s name to a variable named last_student.
 
 def manage_students():
-    # your code here
-
+    students = ['John', 'Paul', 'George', 'Ringo']
+    first_name = students[1]
+    last_name = students[-1]
+    return first_name, last_name
 # Call the function and print the result
 print('Exercise 1:', manage_students())
 
 # Exercise 2: Loop and String Concatenation
 #
-# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Create a tuple named foods containing the same number of foods 
+# (strings) as there are names in the students list.
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
 
 def combine_foods():
-    # your code here
-
+    foods = 'Cheeseburger', 'Double Double', 'Animal Fries', 'Lite Pink Lemonade'
+    meal = ''
+    for food in foods:
+        meal += food + ' - '
+    return meal
 # Call the function and print the result
 print('Exercise 2:', combine_foods())
 
 # Exercise 3: Slicing Tuples
 #
-# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+# Using the slice operator, assign a new tuple containing only the 
+# last two food strings in the foods to a variable named last_two_foods.
 
 def slice_foods():
-    # your code here
+    foods = 'Cheeseburger', 'Double Double', 'Animal Fries', 'Lite Pink Lemonade'
+    last_two_foods = foods[-2:]
+    return last_two_foods
 
 # Call the function and print the result
 print('Exercise 3:', slice_foods())
@@ -53,7 +62,8 @@ print('Exercise 3:', slice_foods())
 # Exercise 4: Dictionaries and String Formatting
 #
 # Create a dictionary named home_town containing the keys of city, state, and population.
-# Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
+# Using the home_town dictionary, assign to a variable named home_town_message 
+# a string with this format: “I was born in <city>, <state> - population of <population>”
 
 def hometown_info():
     # your code here
@@ -64,7 +74,8 @@ print('Exercise 4:', hometown_info())
 # Exercise 5: Iterating Over Dictionary Items
 #
 # Define an empty list named home_town_items.
-# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
+# Use a for loop to iterate over the key: value pairs in the home_town 
+# dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
 def list_home_town_items():
     # your code here
@@ -74,7 +85,8 @@ print('Exercise 5:', list_home_town_items())
 
 # Exercise 6: Celebrate Students
 #
-# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# Using the list of students and a list comprehension, assign to a variable 
+# named awesome_students a new list containing strings.
 # For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
 
 def create_awesome_students():
@@ -85,8 +97,11 @@ print('Exercise 6:', create_awesome_students())
 
 # Exercise 7: Filter Foods
 #
-# Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
-# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
+# Assign to a variable named foods_with_an_a the result of 
+# list comprehension that filters the foods tuple to only 
+# include food strings that contain the letter 'a'.
+# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), 
+# foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
 
 def filter_foods_with_a():
     # your code here
